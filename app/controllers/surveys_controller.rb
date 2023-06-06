792b0c387cdc09e8480c120d44a624af0d16aa1b
knowankit <44323532+knowankit@users.noreply.github.com>
2023-06-06 18:08:07 +0530

    feat: add CRUD connection

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,2 +1,27 @@
 class SurveysController < ApplicationController
+  def index
+    @surveys = Survey.all
+
+    render json: @surveys
+  end
+
+  def create
+    @survey = Survey.create(name: params[:name], permalink: params[:permalink])
+
+    render json: @survey
+  end
+
+  def update
+    @survey = Survey.find(params[:id])
+    @survey = Survey.update(name: params[:name], permalink: params[:permalink])
+
+    render json: @survey
+  end
+
+  def destroy
+    @survey = Survey.find(params[:id])
+    @survey.destroy
+
+    render json: "#{@survey.id} is deleted"
+  end
 end
