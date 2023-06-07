a3e93d73cf7cf4044f10bf5419ecae5a5fd18010
knowankit <44323532+knowankit@users.noreply.github.com>
2023-06-07 16:35:43 +0530

    feat: adds validaton for the field

--- app/controllers/api/v1/surveys_controller.rb
+++ app/controllers/api/v1/surveys_controller.rb
@@ -1,27 +1,31 @@
 class Api::V1::SurveysController < ApplicationController
   def index
-    @surveys = Survey.all
+    surveys = Survey.all
 
-    render json: @surveys
+    render json: surveys
   end
 
   def create
-    @survey = Survey.create(name: params[:name], permalink: params[:permalink])
+    survey = Survey.new(name: params[:name], permalink: params[:permalink])
 
-    render json: @survey
+    if survey.save
+      render json: survey
+    else
+      render json: { errors: survey.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   def update
-    @survey = Survey.find(params[:id])
-    @survey = Survey.update(name: params[:name], permalink: params[:permalink])
+    survey = Survey.find(params[:id])
+    survey = Survey.update(name: params[:name], permalink: params[:permalink])
 
-    render json: @survey
+    render json: survey
   end
 
   def destroy
-    @survey = Survey.find(params[:id])
-    @survey.destroy
+    survey = Survey.find(params[:id])
+    survey.destroy
 
-    render json: "#{@survey.id} is deleted"
+    render json: "#{survey.id} is deleted"
   end
 end
