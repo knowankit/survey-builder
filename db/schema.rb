e9b552fb27f37766afd5137b14bc46a42f19f7a1
knowankit <44323532+knowankit@users.noreply.github.com>
2023-06-08 13:45:12 +0530

    feat: add columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_06_091339) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_08_080938) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_06_091339) do
     t.string "permalink"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "user_id"
+    t.boolean "is_published", default: false
   end
 
 end
