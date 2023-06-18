fee034360b835ff93f251f12f063576e2086fac2
knowankit <44323532+knowankit@users.noreply.github.com>
2023-06-18 21:06:16 +0530

    feat: delete question

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -2,8 +2,7 @@ class Api::V1::QuestionsController < Api::V1::ApplicationController
   before_action :authenticate_user
 
   def index
-    puts "all questions"
-    questions = Question.find_by(survey_id: params[:survey_id], user_id: @current_user.id)
+    questions = Question.find_by(survey_id: params[:survey_id], user_id: @current_user.id) || []
 
     render json: questions
   end
@@ -17,6 +16,22 @@ class Api::V1::QuestionsController < Api::V1::ApplicationController
       render json: question
     else
       render json: { errors: question.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    question = Question.find_by(id: params[:id] ,survey_id: params[:survey_id], user_id: @current_user.id)
+
+    if question
+      if question.is_answered?
+        render json: { error: "Question has already been answered and cannot be deleted" }, status: :unprocessable_entity
+      elsif question.destroy
+        render json: question
+      else
+        render json: { error: question.errors.full_messages }, status: :unprocessable_entity
+      end
+    else
+      render json: { error: "Question not found" }, status: :not_found
     end
   end
 
