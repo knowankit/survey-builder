c2236361310de2406c2dcdc85b238f8372a5373b
knowankit <44323532+knowankit@users.noreply.github.com>
2023-06-18 20:57:29 +0530

    feat: add index and create questions

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -1,2 +1,28 @@
 class Api::V1::QuestionsController < Api::V1::ApplicationController
+  before_action :authenticate_user
+
+  def index
+    puts "all questions"
+    questions = Question.find_by(survey_id: params[:survey_id], user_id: @current_user.id)
+
+    render json: questions
+  end
+
+  def create
+    question = Question.new(question_params)
+    question.user_id = @current_user.id
+    question.survey_id = params[:survey_id]
+
+    if question.save
+      render json: question
+    else
+      render json: { errors: question.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:content, :question_type)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
-  resources :answers
-  resources :responses
-  resources :question_options
+
   namespace :api do
     namespace :v1 do
       resources :sessions, only: [:create]
-      resources :surveys
+      resources :surveys do
         resources :questions
+      end
       resources :users
+      resources :answers
+      resources :responses
+      resources :question_options
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
