97e3d23dcb6f45a2163cb9990a6da51935913d62
knowankit <44323532+knowankit@users.noreply.github.com>
2023-06-18 16:15:21 +0530

    feat: adds user_id column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_17_141151) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_18_104107) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_17_141151) do
     t.bigint "survey_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.index ["survey_id"], name: "index_questions_on_survey_id"
   end
 
