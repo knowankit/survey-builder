95ac0a18205f6faa29150bdb0e1ee19dcb2bddba
knowankit <44323532+knowankit@users.noreply.github.com>
2023-07-05 22:16:05 +0530

    fix: add uniqiness constraint in db

---
+++ db/migrate/20230705150453_add_uniqueness_column_constraint.rb
@@ -0,0 +1,7 @@
+class AddUniquenessColumnConstraint < ActiveRecord::Migration[7.0]
+  def change
+    add_index :surveys, :permalink, unique: true
+    add_index :users, :email, unique: true
+    add_index :users, :username, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_02_140705) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_05_150453) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -75,6 +75,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_02_140705) do
     t.datetime "updated_at", null: false
     t.boolean "is_published", default: false
     t.bigint "user_id", null: false
+    t.index ["permalink"], name: "index_surveys_on_permalink", unique: true
     t.index ["user_id"], name: "index_surveys_on_user_id"
   end
 
@@ -88,6 +89,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_02_140705) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "answers", "question_options"
