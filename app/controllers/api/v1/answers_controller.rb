a22bfca20e8b4c1e2c6fb66290268d3041f78b19
knowankit <44323532+knowankit@users.noreply.github.com>
2023-07-08 14:56:49 +0530

    feat: add answers api

--- app/controllers/api/v1/answers_controller.rb
+++ app/controllers/api/v1/answers_controller.rb
@@ -4,7 +4,28 @@ module Api
   module V1
     # This class handles API requests related to question answers.
     class AnswersController < ApplicationController
-      def create; end
+      def create
+        begin
+          ActiveRecord::Base.transaction do
+            response = Response.new(user_id: @current_user.id, survey_id: params[:survey_id])
+            response.save!
+
+            answers = params[:answers]
+
+            answers.each do |answer_params|
+              answer = Answer.new(answer_params.permit(:question_id, :content, :question_option_id).merge(response_id: response.id, user_id: @current_user.id, survey_id: params[:survey_id]))
+              answer.save!
+            end
+          end
+
+          render json: { message: 'Answers submitted successfully' }, status: :ok
+        rescue ActiveRecord::RecordInvalid => e
+          error_message = e.record.errors.full_messages.join(', ')
+          render json: { error: "Validation error: #{error_message}" }, status: :unprocessable_entity
+        rescue StandardError => e
+          render json: { error: e.message }, status: :unprocessable_entity
+        end
+      end
     end
   end
 end
