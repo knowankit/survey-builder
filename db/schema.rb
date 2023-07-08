24f0e599f5a2baebae5f1a6d5a003b4b430d7b33
knowankit <44323532+knowankit@users.noreply.github.com>
2023-07-08 13:04:15 +0530

    feat: remove submitted column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_06_180942) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_08_073158) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -61,7 +61,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_06_180942) do
   end
 
   create_table "responses", force: :cascade do |t|
-    t.datetime "submitted_at"
     t.bigint "user_id"
     t.bigint "survey_id"
     t.datetime "created_at", null: false
