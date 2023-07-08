a325a6d613d2aed2ae717649a13619d5991947ec
knowankit <44323532+knowankit@users.noreply.github.com>
2023-07-08 19:01:01 +0530

    fix: rubocop

--- app/controllers/api/v1/answers_controller.rb
+++ app/controllers/api/v1/answers_controller.rb
@@ -5,26 +5,44 @@ module Api
     # This class handles API requests related to question answers.
     class AnswersController < ApplicationController
       def create
-        begin
-          ActiveRecord::Base.transaction do
-            response = Response.new(user_id: @current_user.id, survey_id: params[:survey_id])
-            response.save!
+        ActiveRecord::Base.transaction do
+          response = create_response
 
-            answers = params[:answers]
-
-            answers.each do |answer_params|
-              answer = Answer.new(answer_params.permit(:question_id, :content, :question_option_id).merge(response_id: response.id, user_id: @current_user.id, survey_id: params[:survey_id]))
-              answer.save!
-            end
+          params[:answers].each do |answer_params|
+            create_answer(answer_params, response)
           end
-
-          render json: { message: 'Answers submitted successfully' }, status: :ok
-        rescue ActiveRecord::RecordInvalid => e
-          error_message = e.record.errors.full_messages.join(', ')
-          render json: { error: "Validation error: #{error_message}" }, status: :unprocessable_entity
-        rescue StandardError => e
-          render json: { error: e.message }, status: :unprocessable_entity
         end
+
+        render json: { message: 'Answers submitted successfully' }, status: :ok
+      rescue ActiveRecord::RecordInvalid => e
+        render_validation_error(e.record)
+      rescue StandardError => e
+        render_error(e)
+      end
+
+      private
+
+      def create_response
+        Response.create!(user_id: @current_user.id, survey_id: params[:survey_id])
+      end
+
+      def create_answer(answer_params, response)
+        answer_params = answer_params.permit(:question_id, :content, :question_option_id)
+        answer_params = answer_params.merge(
+          response_id: response.id,
+          user_id: @current_user.id,
+          survey_id: params[:survey_id]
+        )
+        Answer.create!(answer_params)
+      end
+
+      def render_validation_error(record)
+        error_message = record.errors.full_messages.join(', ')
+        render json: { error: "Validation error: #{error_message}" }, status: :unprocessable_entity
+      end
+
+      def render_error(error)
+        render json: { error: error.message }, status: :unprocessable_entity
       end
     end
   end
