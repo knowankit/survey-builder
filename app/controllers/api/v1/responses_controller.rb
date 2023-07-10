47c2abfb2987177d9feaadbe0be4ffbca0bc20ee
knowankit <44323532+knowankit@users.noreply.github.com>
2023-07-10 22:54:10 +0530

    feat: get response

--- app/controllers/api/v1/responses_controller.rb
+++ app/controllers/api/v1/responses_controller.rb
@@ -3,7 +3,13 @@
 module Api
   module V1
     # This class handles API requests related to responses of survey.
-    class ResponsesController < Api::V1::ApplicationController
+    class ResponsesController < ApplicationController
+      def show
+        survey = Question.find_by(survey_id: params[:survey_id])
+        response = Answer.where(response_id: params[:id])
+
+        render json: { survey:, response: }
+      end
     end
   end
 end
