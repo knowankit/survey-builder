42ab4c41b3db4ba564d5993c207076055a736419
knowankit <44323532+knowankit@users.noreply.github.com>
2023-08-24 16:01:05 +0530

    feat: add response API

--- app/controllers/api/v1/responses_controller.rb
+++ app/controllers/api/v1/responses_controller.rb
@@ -5,10 +5,34 @@ module Api
     # This class handles API requests related to responses of survey.
     class ResponsesController < ApplicationController
       def show
-        survey = Question.find_by(survey_id: params[:survey_id])
-        response = Answer.where(response_id: params[:id])
+        # survey = Question.find_by(survey_id: params[:survey_id])
+        # response = Answer.where(response_id: params[:id])
 
-        render json: { survey:, response: }
+        # render json: { response: }
+
+        survey = Survey.find(params[:survey_id])
+        response = Response.find(params[:id])
+
+        questions_with_answers = survey.questions.map do |question|
+          answers = response.answers.where(question_id: question.id)
+          {
+            question: question,
+            answers: answers
+          }
+        end
+
+        render json: questions_with_answers
+      end
+
+      def index
+        default_per_page = 10
+        default_page = 1
+        per_page = params.fetch(:per_page, default_per_page).to_i
+        page = params.fetch(:page, default_page).to_i
+
+        responses = Response.where(user_id: @current_user.id).page(page).per(per_page)
+
+        render json: { responses:, page:, per_page: }
       end
     end
   end
--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,7 @@ module SurveyBuilder
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
     config.middleware.use ActionDispatch::Cookies
+    config.action_controller.default_protect_from_forgery = true
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
